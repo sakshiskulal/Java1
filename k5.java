9f6e5c187074ceced240ebf40c1ce58f13a6c6fa
sakshiskulal <sakshi.23cs131@sode-edu.in>
2024-09-17 16:04:51 +0530

    Add files via upload

---
+++ k5.java
@@ -0,0 +1,11 @@
+public class k5{
+    public static void main(String[] args){
+        int num1=Integer.parseInt(args[0]);
+        int factn=1;
+	for (int i=1;i<num1+1;i++){
+	    factn=factn*i;
+	}
+	System.out.println(factn);
+	
+    }
+}
