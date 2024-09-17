c2b9de60b9eefe5b1fa8e75a27531d097d883e35
sakshiskulal <sakshi.23cs131@sode-edu.in>
2024-09-17 18:56:36 +0530

    Add files via upload

---
+++ k6.java
@@ -0,0 +1,7 @@
+public class k6{
+    public static void main(String[] args){
+	System.out.println("3" + "*" + "1" + "=" + "3");
+	System.out.println("3" + "*" + "2" + "=" + "6");
+	System.out.println("3" + "*" + "3" + "=" + "9");
+   }
+}
