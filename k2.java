98d7acad496e8a298508ca9105837fe2231b8a2f
sakshiskulal <sakshi.23cs131@sode-edu.in>
2024-09-17 15:52:20 +0530

    Add files via upload

---
+++ k2.java
@@ -0,0 +1,12 @@
+public class k2{
+    public static void main(String[] args){
+        int n1=8;
+	int n2=4;
+	int sum1=n1+n2;
+	int dif1=n1-n2;
+	int prd1=n1*n2;
+	int div1=n1/n2;
+	int rem1=n1%n2;
+	System.out.println(sum1);
+    }
+}
