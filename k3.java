6a6ad1bf6dd55802e45f9d701709c309efa19fc7
sakshiskulal <sakshi.23cs131@sode-edu.in>
2024-09-17 15:55:17 +0530

    Add files via upload

---
+++ k3.java
@@ -0,0 +1,8 @@
+public class k3{
+    public static void main(String[] args){
+        int n1=Integer.parseInt(args[0]);
+	int n2=Integer.parseInt(args[1]);
+	int sum1=n1+n2;
+	System.out.println(sum1);
+    }
+}
